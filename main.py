c59b70c07d10b24e1da3b8db2872bd457597ed05
Bobby Ong <bobbyongce@gmail.com>
2012-07-08 17:51:33 +0100

    initial css bootstrap commit

--- main.py
+++ main.py
@@ -14,11 +14,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import jinja2
+import logging
+
+##### Website Page Handlers #####
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class PageHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+##### Main Page #####
+
+class MainHandler(PageHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        self.render('main.html')
+
+
+##### URL Mapping #####
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
                               debug=True)
